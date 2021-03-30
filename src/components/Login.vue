7703f95269026bac91328647060f756e0090b808
phantomassassin1109 <948949782@qq.com>
2021-03-30 18:54:00 +0800

    login layout

---
+++ src/components/Login.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="login_container">
+    <div class="login_box"></div>
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style scoped>
+.login_container {
+  background: #2b4b6b;
+  height: 100%
+}
+
+.login_box {
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 5px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%)
+}
+</style>
